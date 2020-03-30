80b7fb1a870b5bb07b1f5e65f8f9f49fc5ee392d
Hang (Anna) Li <annlihappy@gmail.com>
2020-03-30 19:23:58 -0400

    add game object type enums

---
+++ Scripts/enums/GameObjectTypes.js
@@ -0,0 +1,21 @@
+"use strict";
+var enums;
+(function (enums) {
+    var GameObjectTypes;
+    (function (GameObjectTypes) {
+        GameObjectTypes[GameObjectTypes["BULLET"] = 0] = "BULLET";
+        GameObjectTypes[GameObjectTypes["BUTTON"] = 1] = "BUTTON";
+        GameObjectTypes[GameObjectTypes["LIVEICON"] = 2] = "LIVEICON";
+        GameObjectTypes[GameObjectTypes["STARICON"] = 3] = "STARICON";
+        GameObjectTypes[GameObjectTypes["MONSTERA"] = 4] = "MONSTERA";
+        GameObjectTypes[GameObjectTypes["MONSTERB"] = 5] = "MONSTERB";
+        GameObjectTypes[GameObjectTypes["MONSTERC"] = 6] = "MONSTERC";
+        GameObjectTypes[GameObjectTypes["MONSTERD"] = 7] = "MONSTERD";
+        GameObjectTypes[GameObjectTypes["PLANET"] = 8] = "PLANET";
+        GameObjectTypes[GameObjectTypes["SPACESHIP"] = 9] = "SPACESHIP";
+        GameObjectTypes[GameObjectTypes["UNIVERSE"] = 10] = "UNIVERSE";
+        GameObjectTypes[GameObjectTypes["UNDEFINED"] = 11] = "UNDEFINED";
+        GameObjectTypes[GameObjectTypes["NUM_OF_TYPES"] = 12] = "NUM_OF_TYPES";
+    })(GameObjectTypes = enums.GameObjectTypes || (enums.GameObjectTypes = {}));
+})(enums || (enums = {}));
+//# sourceMappingURL=GameObjectTypes.js.map
---
+++ Scripts/enums/GameObjectTypes.ts
@@ -0,0 +1,17 @@
+module enums {
+  export enum GameObjectTypes {
+    BULLET,
+    BUTTON,
+    LIVEICON,
+    STARICON,
+    MONSTERA,
+    MONSTERB,
+    MONSTERC,
+    MONSTERD,
+    PLANET,
+    SPACESHIP,
+    UNIVERSE,
+    UNDEFINED,
+    NUM_OF_TYPES
+  }
+}
